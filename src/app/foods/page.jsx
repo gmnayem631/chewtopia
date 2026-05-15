a6fa85ce1553adcabcde9993a0d91875a072646d
gmnayem631 <gmnayem631@gmail.com>
2026-05-15 15:21:23 +0600

    added basic routes

---
+++ src/app/foods/page.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const FoodsPage = () => {
+  return <div className="py-10">Foods Page</div>;
+};
+
+export default FoodsPage;
---
+++ src/app/reviews/page.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ReviewsPage = () => {
+  return <div className="py-10">Reviews Page</div>;
+};
+
+export default ReviewsPage;
