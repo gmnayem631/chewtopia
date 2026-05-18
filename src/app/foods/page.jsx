58fdd46917cfeb72695eebe528ae8ecd502e26fb
gmnayem631 <gmnayem631@gmail.com>
2026-05-18 21:54:32 +0600

    fixed fetch api bug

--- src/app/foods/page.jsx
+++ src/app/foods/page.jsx
@@ -1,8 +1,9 @@
+import FoodCard from "@/components/cards/FoodCard";
 import React from "react";
 
 const getFoods = async () => {
   const res = await fetch(
-    " https://taxi-kitchen-api.vercel.app/api/v1/foods/random",
+    "https://taxi-kitchen-api.vercel.app/api/v1/foods/random",
   );
   const data = await res.json();
   return data.foods || [];
@@ -13,6 +14,12 @@ const FoodsPage = async () => {
   return (
     <div className="">
       <h2 className="font-bold text-4xl">Total {foods.length} foods found</h2>
+
+      <div className="grid grid-cols-3 gap-5 my-5">
+        {foods.map((food) => (
+          <FoodCard key={food.id} food={food}></FoodCard>
+        ))}
+      </div>
     </div>
   );
 };
