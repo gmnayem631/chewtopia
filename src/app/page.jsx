2f9da74916da0feb305c05ce0cbffc686d452f63
gmnayem631 <gmnayem631@gmail.com>
2026-05-24 14:56:26 +0600

    branch test: added accordion to home page

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,5 +1,10 @@
-import Image from "next/image";
+import FAQ from "../components/faq/FAQ";
 
 export default function Home() {
-  return <div>Home Page</div>;
+  return (
+    <div>
+      Home Page
+      <FAQ></FAQ>
+    </div>
+  );
 }
